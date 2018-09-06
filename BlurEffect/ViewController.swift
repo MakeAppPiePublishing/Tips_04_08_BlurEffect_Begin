d82a60dd34f008516e789e9feb235eea620eadfa
Steve Lipton <steve@makeapppie.com>
2018-09-06 07:55:21 -0500

    Initial commit of the Example file

--- BlurEffect/ViewController.swift
+++ BlurEffect/ViewController.swift
@@ -2,13 +2,35 @@
 //  ViewController.swift
 //  BlurEffect
 //
-//  Created by Steven Lipton on 9/6/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+//  An exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    // A function to layout a view behind another view
+    func layout(background:UIView, behind view:UIView, padding:CGFloat){
+        var constraints = [NSLayoutConstraint]()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        background.translatesAutoresizingMaskIntoConstraints = false
+        // view.centerX = background.centerX
+        constraints += [NSLayoutConstraint.init(item: view, attribute: .centerX, relatedBy: .equal, toItem: background, attribute: .centerX, multiplier: 1.0, constant: 0)]
+        // view.centerY = Background.centerY
+        constraints += [NSLayoutConstraint.init(item: view, attribute: .centerY, relatedBy: .equal, toItem: background, attribute: .centerY, multiplier: 1.0, constant: 0)]
+        // background.width = view.width + (padding * 2.0)
+        constraints += [NSLayoutConstraint.init(item: background, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1.0, constant: padding * 2.0)]
+        // background.height = view.height + (padding * 2.0)
+        constraints += [NSLayoutConstraint.init(item: background, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: padding * 2.0)]
+        NSLayoutConstraint.activate(constraints)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
